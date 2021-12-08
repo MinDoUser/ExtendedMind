b7419a28d4d625228ea5bfedc48f6f02a7186cc1
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-12-08 16:33:58 +0100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+//require(Nothing);
+
+function health(){
+  let player = Vars.player.unit();
+     if(player != null && player.type != null){
+  return "Health: "+player.health+"/"+player.maxHealth
+     }
+};
+
+let canAdd = true;
+
+Events.on(ClientLoadEvent, () => {
+    Events.on(WorldLoadEvent, () => {
+	if(canAdd){
+		let m = Vars.mobile;
+     let healthUI = Vars.ui.hudGroup.children.get(5).children.get(m ? 2 : 0).children.get(0).children.get(0).children.get(0);
+         healthUI.row();
+	       healthUI.label(() => health())
+	            .visible(true)
+	            .touchable(Touchable.disabled)
+	            .name("Health")
+	            .top().right();
+		canAdd = false;
+	}
+    });
+});
