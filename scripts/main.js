4655575809bb85034f6024c8390381c1d52bc3f4
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-01-11 11:06:48 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,6 @@
 require("blocks/crafter/fuser");
 
-let name = "";
-
 Events.on(ClientLoadEvent, () => {
-  Vars.content.blocks().each(e => {
-    name = ("[white]"+e.localizedName, Styles.techLabel)
-    e.localizedName = name;
-  });
     
 var dialog = new BaseDialog("ExtendMind | Welcome Screen");
 
@@ -20,7 +14,8 @@ var dialog = new BaseDialog("ExtendMind | Welcome Screen");
         var table = new Table();
         table.add("[white]<< Loaded Extended Mind >>", Styles.techLabel).row();
         table.row();
-		    table.add("This Mod is still in Development! \n It adds some intresting things and other senceless stuff. \n Have Fun!").left().growX().wrap().width(620).maxWidth(620).pad(4).labelAlign(Align.left);
+	table.add("This Mod is still in Development! \n It adds some intresting things and other senceless stuff. \n Have Fun!").left().growX().wrap().width(620).maxWidth(620).pad(4).labelAlign(Align.left);
+	table.row();
         table.add("[cyan]> By lol02 <", Styles.techLabel).row();
       return table;
     })()).grow().center().maxWidth(620);
