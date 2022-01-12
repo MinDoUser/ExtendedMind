c3f22336369abb2f8c0e6b1f2e83a6606084f5da
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-01-12 15:38:34 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,27 @@
 require("blocks/crafter/fuser");
 
+var o = 1;
+
 Events.on(ClientLoadEvent, () => {
     
-var dialog = new BaseDialog("ExtendMind | Welcome Screen");
+var credits = new BaseDialog("ExtendMind | Credits");
+
+    credits.buttons.defaults().size(210, 64);
+    credits.buttons.button("@close", run(() => {
+        dialog.hide();
+    })).size(210, 64);
+
+    credits.cont.pane((() => {
+
+        var table = new Table();
+        table.add("[white]<< Credits >>", Styles.techLabel).row();
+        table.row();
+	table.add("Following People helped me with that mods: \n[cyan]lol02[]: Author of this Mod, and main scripter. Also does all thus sprites.\n [stat]Rine: Giving ideas and helping me with that Mod.");
+      return table;
+    })()).grow().center().maxWidth(620);
+    credits.show();
+	
+	var dialog = new BaseDialog("ExtendMind | Welcome Screen");
 
     dialog.buttons.defaults().size(210, 64);
     dialog.buttons.button("@close", run(() => {
@@ -20,5 +39,35 @@ var dialog = new BaseDialog("ExtendMind | Welcome Screen");
       return table;
     })()).grow().center().maxWidth(620);
     dialog.show();
+	
+	var infoDialog = new BaseDialog("ExtendMind | Info Screen");
+
+    infoDialog.buttons.defaults().size(210, 64);
+    infoDialog.buttons.button("@close", run(() => {
+        infoDialog.hide();
+    })).size(210, 64);
+	infoDialog.buttons.button(("Credits"), Styles.techLabel, () => {
+                credits.show();
+            });
+
+    infoDialog.cont.pane((() => {
+
+        var it = new Table();
+        	it.add("[white]<< Extended Mind Info >>", Styles.techLabel).row();
+		it.row();
+		it.add("[stat]< General Information >",Styles.techLabel).row();
+	    	it.add("This Mod is my first Mod on GitHub. It's still [scarlet]WIP[], so don't worry about unfinished parts.").row();
+	    	it.add("This Mod was also created, because I wanted to learn, how to use JS in Mods, so there are may some errors and [scarlet]CRASHES[].").row().row();
+	   	it.add("[stat]< Blocks and Items >",Styles.techLabel).row();
+	    	it.add("There are some funny and special as well as useful blocks and items. I think you try them yourself.").row();
+	    	it.add("[cyan]Thanks for Playing!",Styles.techLabel).row();
+      return it;
+    })()).grow().center().maxWidth(620);
+	
+	Vars.ui.custom.buttons.button("Info", Styles.techLabel,()=>{
+		Log.info("You opened info-tab "+o+"times.");
+		infoDialog.show();
+		o++;
+	});
 });
 
