4e957b0372d68b13c16d53723f215b8c47829b61
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-01-12 15:20:20 +0100

    Update fuser.js

--- scripts/blocks/crafter/fuser.js
+++ scripts/blocks/crafter/fuser.js
@@ -14,11 +14,11 @@ const fuser = extend(GenericCrafter, "tangen-fuser", {
 })
 fuser.buildType = () => extend(GenericCrafter.GenericCrafterBuild, fuser, {
     draw(){
-      Draw.rect(this.region, this.x, this.y);
+      Draw.rect(fuser.region, this.x, this.y);
       Draw.alpha(Mathf.tan(this.progress, 40, 4 / 3 * 4));
-      Draw.rect(this.itemRegion, this.x, this.y);
+      Draw.rect(fuser.itemRegion, this.x, this.y);
       Draw.alpha(1);
       Lines.circle(this.x, this.y, 2 *Mathf.tan(this.progress, 40, 4 / 3 * 4)*3);
-      Draw.rect(this.topRegion, this.x, this.y);
+      Draw.rect(fuser.topRegion, this.x, this.y);
     }
 })
