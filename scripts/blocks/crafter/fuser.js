a8501f09d78991d490c2258e35d58f3cce086747
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-01-13 17:56:48 +0100

    Update fuser.js

--- scripts/blocks/crafter/fuser.js
+++ scripts/blocks/crafter/fuser.js
@@ -1,13 +1,13 @@
 const fuser = extend(GenericCrafter, "tangen-fuser", {
     load(){
         this.super$load()
-        this.region = Core.atlas.find(this.name + "-bottom");
+        this.region = Core.atlas.find(this.name + "-base");
         this.topRegion = Core.atlas.find(this.name + "-top");
         this.itemRegion = Core.atlas.find(this.name + "-item");
     },
   	icons: function(){
 	    return [
-		    Core.atlas.find(this.name + "-bottom"),
+		    Core.atlas.find(this.name + "-base"),
 		    Core.atlas.find(this.name + "-top")
 	    ]
     }
@@ -18,7 +18,7 @@ fuser.buildType = () => extend(GenericCrafter.GenericCrafterBuild, fuser, {
       Draw.alpha(Mathf.tan(this.progress, 40, 4 / 3 * 4));
       Draw.rect(fuser.itemRegion, this.x, this.y);
       Draw.alpha(1);
-      Lines.circle(this.x, this.y, 2 *Mathf.tan(this.progress, 40, 4 / 3 * 4)*3);
+      Lines.circle(this.x, this.y, 2 *Mathf.tan(this.progress, 40, 4 / 3 * 4)*7);
       Draw.rect(fuser.topRegion, this.x, this.y);
     }
 })
