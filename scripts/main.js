cffbe9c0ebc8d97c44a5cb711017a364a4f9609d
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-02-15 18:23:39 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 //require("blocks/crafter/fuser"); No.
-
-var o = 1;
-
+require("EMSettings");
 Events.on(ClientLoadEvent, () => {
 	
 	var dialog = new BaseDialog("ExtendMind | Welcome Screen");
@@ -22,32 +20,5 @@ Events.on(ClientLoadEvent, () => {
       return table;
     })()).grow().center().maxWidth(620);
     dialog.show();
-	
-	var infoDialog = new BaseDialog("ExtendMind | Info Screen");
-
-    infoDialog.buttons.defaults().size(210, 64);
-    infoDialog.buttons.button("@close", run(() => {
-        infoDialog.hide();
-    })).size(210, 64);
-
-    infoDialog.cont.pane((() => {
-
-        var it = new Table();
-        	it.add("[white]<< Extended Mind Info >>", Styles.techLabel).row();
-		it.row();
-		it.add("[stat]< General Information >",Styles.techLabel).row();
-	    	it.add("This Mod is my first Mod on GitHub. It's still [scarlet]WIP[], so don't worry about unfinished parts.").row();
-	    	it.add("This Mod was also created, because I wanted to learn, how to use JS in Mods,\n so there are may some errors and [scarlet]CRASHES[].").row();
-	   	it.add("[stat]< Blocks and Items >",Styles.techLabel).row();
-	    	it.add("There are some funny and special as well as useful blocks and items. I think you try them yourself.").row();
-	    	it.add("[cyan]Thanks for Playing!",Styles.techLabel).row();
-      return it;
-    })()).grow().center().maxWidth(1000);
-	
-	Vars.ui.custom.buttons.button("Info", Icon.info,()=>{
-		Log.info("You opened [stat]ExtendedMind Info Tab[] "+o+" times.");
-		infoDialog.show();
-		o++;
-	});
 });
 
